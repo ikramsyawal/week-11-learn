bc773b818e1b3b8ac5fd1b6ee6bebfa89556c9e1
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-03-22 17:18:53 +0800

    success in creating docker images

--- config/config.js
+++ config/config.js
@@ -2,17 +2,33 @@ require("dotenv").config();
 
 const config = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     host: "localhost",
     dialect: "postgres",
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     database: process.env.DB_TEST_NAME,
     host: "localhost",
+    dialect: "postgres",
+  },
+
+  docker: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+  },
+  dockerTest: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
---
+++ dockerfile
@@ -0,0 +1,18 @@
+FROM node:lts-alpine3.19
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["npm", "run", "docker"]
+
+
+
+
+
